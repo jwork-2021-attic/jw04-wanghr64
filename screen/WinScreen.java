d1fe9e06ac63c3c846ac709fd52564ab0d192d28
wanghr <239955612@qq.com>
2021-10-19 11:24:01 +0800

    better UI

--- screen/WinScreen.java
+++ screen/WinScreen.java
@@ -24,10 +24,16 @@ import asciiPanel.AsciiPanel;
  * @author Aeranythe Echosong
  */
 public class WinScreen extends RestartScreen {
-
     @Override
     public void displayOutput(AsciiPanel terminal) {
-        terminal.write("You won! Press enter to go again.", 0, 0);
+        terminal.write("                                                              dP ", 8, 3);
+        terminal.write("                                                              88 ", 8, 4);
+        terminal.write("dP    dP .d8888b. dP    dP    dP  dP  dP .d8888b. 88d888b.    88 ", 8, 5);
+        terminal.write("88    88 88'  `88 88    88    88  88  88 88'  `88 88'  `88    dP ", 8, 6);
+        terminal.write("88.  .88 88.  .88 88.  .88    88.88b.88' 88.  .88 88    88       ", 8, 7);
+        terminal.write("`8888P88 `88888P' `88888P'    8888P Y8P  `88888P' dP    dP    oo ", 8, 8);
+        terminal.write("     .88                                                         ", 8, 9);
+        terminal.write(" d8888P                                                          ", 8, 10);
+        terminal.write("Press Enter to restart...", 0, 15);
     }
-
 }
